5208d2c918f9a36e30b8f1b1e431bd331470cfbf
unknown <luccasdrapala@gmail.com>
2023-03-19 10:26:13 -0300

    configurando rotas com regex :poop:

--- app/router/router.php
+++ app/router/router.php
@@ -8,7 +8,7 @@ function routes():array
 function verifyUriRoute($uri, $routes)
 {
     if(array_key_exists($uri, $routes)){
-        return [];
+        return [$uri => $routes[$uri]];
     }
     return [];
 }
@@ -19,5 +19,17 @@ function router()
 
     $routes = routes();
 
-    verifyUriRoute($uri, $routes);
+    $matchedUri = verifyUriRoute($uri, $routes);
+
+    if(empty($matchedUri)){
+        $matchedUri = array_filter(
+            array_keys($routes),
+            function($value) use($uri){
+                $regex = str_replace('/', '\/', ltrim($value, '/'));
+                return preg_match("/^$regex$/", trim($uri, '/'));
+            }
+        );
+    }
+    
+    var_dump($matchedUri);
 }
