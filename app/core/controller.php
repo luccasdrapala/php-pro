cec400d8d5bfa8d88c7149f65e4584c8780feabb
unknown <luccasdrapala@gmail.com>
2023-03-25 16:23:16 -0300

    iniciando direcionamento para controlleras

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,5 +1,13 @@
 <?php 
 
-function callController($matchedUri){
+function callController ($matchedUri) {
 
+    [$controller, $method] = explode('@', array_values($matchedUri)[0]); // abreviação do methodo list
+
+    if (class_exists($controller)) {
+        var_dump('exist');
+    } else {
+        var_dump('dont exist');    
+    }
+    
 }
