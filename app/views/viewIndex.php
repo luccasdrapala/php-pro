f1b9c6de963c2c8f48a49b9e761e488f421e1dae
unknown <luccasdrapala@gmail.com>
2023-03-29 20:45:21 -0300

    adicionando css

--- app/views/viewIndex.php
+++ app/views/viewIndex.php
@@ -5,10 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$data['title']?></title>
+
+    <link rel="stylesheet" href="/assets/css/styles.css">
+
 </head>
 <body>
 
-    <div>
+    <div class="header">
+        <?php require 'includes/header.php';?>
+    </div>
+
+    <div class="container">
         <?php require ROOT. '/app/views/'. $data['view']?>
     </div>
     
