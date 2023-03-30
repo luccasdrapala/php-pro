f2afd2cbb6d8710d10b22c1d0fc823b5fe491147
unknown <luccasdrapala@gmail.com>
2023-03-29 21:25:12 -0300

    tapa no css

--- app/views/home.php
+++ app/views/home.php
@@ -1,7 +1,7 @@
 <h2>HOME</h2>
 <br>
 
-<ul>
+<ul id="users_home">
     <?php foreach($users as $user): ?>
         <li><?php echo $user->name?> | <a href="/user/<?php echo $user->id?>">Detalhes</a></li>
     <?php endforeach; ?>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,1 +1,9 @@
-<h2>HEADER</h2>
+<ul id="menu_list">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Login</a></li>
+    <li><a href="/">Create</a></li>
+</ul>
+
+<div class="status_login">
+    Bem vindo, visitante !
+</div>
