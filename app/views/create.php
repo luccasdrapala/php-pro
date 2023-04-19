2ec8701a88a0bd400f5fa66ba9cfde4a2c18cd03
unknown <luccasdrapala@gmail.com>
2023-04-18 21:27:16 -0300

    adicionando formulário

---
+++ app/views/create.php
@@ -0,0 +1,29 @@
+<h2>Cadastrar Usuário</h2>
+
+<?= getflash('message')?>
+
+<form action="../pages/forms/form-create-user.php" method="post">
+
+<div class="form-group">
+    <label for="name">Nome</label>
+    <input type="text" class="form-control" name="name" >
+</div>
+
+<div class="form-group">
+    <label for="lastname">Sobrenome</label>
+    <input type="text" class="form-control" name="lastname" >
+</div>
+
+<div class="form-group">
+    <label for="email">Email</label>
+    <input type="text" class="form-control" name="email" >
+</div>
+
+<div class="form-group">
+    <label for="password">Senha</label>
+    <input type="password" class="form-control" name="password" >
+</div>
+
+<button class="btn btn-success">Submit</button>
+
+</form>
