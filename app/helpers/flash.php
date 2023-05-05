e5b695af17381acee897f8a1cb65f18436b7e46d
unknown <luccasdrapala@gmail.com>
2023-05-04 21:02:04 -0300

    do nada funcionou as flash

--- app/helpers/flash.php
+++ app/helpers/flash.php
@@ -2,13 +2,16 @@
 
 function setFlash($index, $message) 
 {
-    $_SESSION['flash'][$index] = $message;
+    if (!isset($_SESSION['flash'][$index])) {
+        $_SESSION['flash'][$index] = $message;
+    }
 }
 
 function getFlash($index, $style = "color:red;") 
 {
     if (isset($_SESSION['flash'][$index])) {
         $flash = $_SESSION['flash'][$index];
+        unset($_SESSION['flash'][$index]);
         return "<span style='{$style}'>{$flash}</span>";
     } 
 }
