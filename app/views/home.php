a9aad42c97c3c97636335de0d9078a937e1f87f1
unknown <luccasdrapala@gmail.com>
2023-05-17 21:16:05 -0300

    terminando configuiração babel com webpack

--- app/views/home.php
+++ app/views/home.php
@@ -8,14 +8,3 @@
         <li><?php echo $user->name?> | <a href="/user/<?php echo $user->id?>">Detalhes</a></li>
     <?php endforeach; ?>
 </ul>
-
-<?php $this->start('scripts') ?>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script>
-
-    
-
-</script>
-
-<?php $this->stop() ?>
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
      mode: process.env.NODE_ENV == 'development' ? 'development' : 'production',
      devtool: process.env.NODE_ENV == 'development' ? 'source-map' : '',
      entry: {
-          app: ['./public/assets/js/app.js'],
+          app: ['@babel/polyfill', './public/assets/js/app.js'],
      },
      output: {
           path: path.resolve(__dirname, 'public'),
