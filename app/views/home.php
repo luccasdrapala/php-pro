f5dfff757871825322a43e742e0b56a09130e85f
unknown <luccasdrapala@gmail.com>
2023-05-18 21:14:29 -0300

    instalando e configurando alpine.js

--- app/views/home.php
+++ app/views/home.php
@@ -3,6 +3,14 @@
 <h2>HOME</h2>
 <br>
 
+<div x-data="users" x-init="loadUsers()">
+    <ul>
+        <template x-for="user in data">
+            <li x-text="user.name"></li>
+        </template>
+    </ul>
+</div>
+
 <ul id="users_home">
     <?php foreach($users as $user): ?>
         <li><?php echo $user->name?> | <a href="/user/<?php echo $user->id?>">Detalhes</a></li>
