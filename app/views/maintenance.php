0ce958c6829f68d0f2a5319bd1c4c8874ea4cf21
unknown <luccasdrapala@gmail.com>
2023-06-26 21:46:02 -0300

    Ajustes para CSS do site em anutencao

--- app/views/maintenance.php
+++ app/views/maintenance.php
@@ -1,3 +1,10 @@
 <?php $this->layout('viewIndex', ['title' => 'maintenance']); ?>
 
+<?php $this->start('css') ?>
+
+<!-- Estilizar o css do site em manutencao -->
+<link rel="stylesheet" href="style.css">    
+
+<?php $this->stop() ?>
+
 <h2 class="h2_header">Em manutencao</h2>
